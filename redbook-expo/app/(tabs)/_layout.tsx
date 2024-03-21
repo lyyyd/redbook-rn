d08015806f959e2ac0aeb7518945d4c449c37ff7
yanding.li <David.Jackson.Lyd@gmail.com>
2024-03-21 21:57:01 +0800

    annotation proxy

--- redbook-expo/app/(tabs)/_layout.tsx
+++ redbook-expo/app/(tabs)/_layout.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, Tabs } from "expo-router";
-import { Image, Text, Pressable, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Image,
+  Text,
+  Pressable,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
 import { useClientOnlyValue } from "@/components/useClientOnlyValue";
-import { ImagePickerResponse, launchImageLibrary } from "react-native-image-picker";
-import icon_tab_publish from '@/assets/img/icon_tab_publish.png';
+import {
+  ImagePickerResponse,
+  launchImageLibrary,
+} from "react-native-image-picker";
+import icon_tab_publish from "@/assets/img/icon_tab_publish.png";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -20,51 +29,51 @@ function TabBarIcon(props: {
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const onPublishPress = () => {
-        launchImageLibrary(
-          {
-            mediaType: 'photo',
-            quality: 1,
-            includeBase64: true,
-          },
-          (res: ImagePickerResponse) => {
-            const {assets} = res;
-            if (!assets?.length) {
-              console.log('选择图片失败');
-              return;
-            }
-            const {uri, width, height, fileName, fileSize, type} = assets[0];
-            console.log(`uri=${uri}, width=${width}, height=${height}`);
-            console.log(
-              `fileName=${fileName}, fileSize=${fileSize}, type=${type}`,
-            );
-          },
-        );
+    launchImageLibrary(
+      {
+        mediaType: "photo",
+        quality: 1,
+        includeBase64: true,
+      },
+      (res: ImagePickerResponse) => {
+        const { assets } = res;
+        if (!assets?.length) {
+          console.log("选择图片失败");
+          return;
+        }
+        const { uri, width, height, fileName, fileSize, type } = assets[0];
+        console.log(`uri=${uri}, width=${width}, height=${height}`);
+        console.log(`fileName=${fileName}, fileSize=${fileSize}, type=${type}`);
+      }
+    );
   };
   const tabBarLabel = (title: string, focused = false) => (
     <Text
       style={{
         fontSize: focused ? 16 : 16,
-        color: focused ? '#333' : '#999',
-        fontWeight: focused ? 'bold' : 'normal',
+        color: focused ? "#333" : "#999",
+        fontWeight: focused ? "bold" : "normal",
         marginBottom: 10,
-      }}>
+      }}
+    >
       {title}
     </Text>
-  )
-  
+  );
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
-          title: '首页',
-          tabBarLabel: ({ focused }) => tabBarLabel('首页', focused),
+          title: "首页",
+          tabBarLabel: ({ focused }) => tabBarLabel("首页", focused),
           // tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           tabBarIcon: ({ color }) => <></>,
           headerShown: false,
@@ -73,8 +82,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="shop"
         options={{
-          title: '购物',
-          tabBarLabel: ({ focused }) => tabBarLabel('购物', focused),
+          title: "购物",
+          tabBarLabel: ({ focused }) => tabBarLabel("购物", focused),
           // tabBarIcon: ({ color }) => <TabBarIcon name="shopping-bag" color={color} />,
           tabBarIcon: ({ color }) => <></>,
           headerShown: false,
@@ -83,24 +92,24 @@ export default function TabLayout() {
       <Tabs.Screen
         name="publish"
         options={{
-          title: '',
+          title: "",
           tabBarShowLabel: false,
-          tabBarIcon: ({ color }) => (<TouchableOpacity
-                key='publish'
-                onPress={onPublishPress}>
-                <Image
-                  style={styles.icon_tab_publish}
-                  source={icon_tab_publish}
-                />
-              </TouchableOpacity>),
+          tabBarIcon: ({ color }) => (
+            <TouchableOpacity key="publish" onPress={onPublishPress}>
+              <Image
+                style={styles.icon_tab_publish}
+                source={icon_tab_publish}
+              />
+            </TouchableOpacity>
+          ),
           headerShown: false,
         }}
       />
       <Tabs.Screen
         name="message"
         options={{
-          title: '消息',
-          tabBarLabel: ({ focused }) => tabBarLabel('消息', focused),
+          title: "消息",
+          tabBarLabel: ({ focused }) => tabBarLabel("消息", focused),
           // tabBarIcon: ({ color }) => <TabBarIcon name="info-circle" color={color} />,
           tabBarIcon: ({ color }) => <></>,
           headerShown: false,
@@ -109,8 +118,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="mine"
         options={{
-          title: '我',
-          tabBarLabel: ({ focused }) => tabBarLabel('我', focused),
+          title: "我",
+          tabBarLabel: ({ focused }) => tabBarLabel("我", focused),
           // tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
           tabBarIcon: ({ color }) => <></>,
           headerShown: false,
@@ -119,7 +128,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="articleDetail"
         options={{
-          title: '',
+          title: "",
           href: null,
           headerShown: false,
         }}
@@ -127,7 +136,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="searchGoods"
         options={{
-          title: '',
+          title: "",
           href: null,
           headerShown: false,
         }}
@@ -138,25 +147,25 @@ export default function TabLayout() {
 
 const styles = StyleSheet.create({
   root: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   tabBarContainer: {
-    width: '100%',
+    width: "100%",
     height: 52,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
   },
   tabItem: {
-    height: '100%',
+    height: "100%",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   icon_tab_publish: {
     width: 58,
     height: 42,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
 });
--- redbookserver/config/config.default.ts
+++ redbookserver/config/config.default.ts
@@ -7,10 +7,10 @@ export default (appInfo: EggAppInfo) => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1676971441623_2539'
 
-  config.cors = {
-    origin: '*', //域名+端口 或者  *(全匹配)
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
-  }
+  // config.cors = {
+  //   origin: '*', //域名+端口 或者  *(全匹配)
+  //   allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  // }
 
   // add your egg config in here
   config.middleware = []
